78941338974b6e3c4ff06da087ed0a95a4c49064
rsys-arawat <108254057+rsys-arawat@users.noreply.github.com>
2022-10-12 15:25:25 +0530

    Update haptik.js

--- haptik.js
+++ haptik.js
@@ -1,87 +1,4 @@
 
-const axios = require("axios");
-const express = require("express");
-
-const app = express();
-
-app.use(express.json());
-
-let counter =0;
-let i = 0;
-let start_time, end_time, callback_time;
-const MAX_COUNT = 5;
-
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-var qs = require("qs");
-
-const port =  3000;
-
-app.listen(port, () => {
-  console.log(`Node server is running at http://localhost:${port}`);
-});
-
-app.get("/count", function (req, res) {
-  console.log("Printing parameters received for / (GET) ", req.query);
-  counter =1;
-  // set response header
-  myFunction();
-
-  res.status = 200;
-
-  res.header("Content-Type", "text/xml");
-
-  // set response content
-
-  res.send("<response>Hi there !</response>");
-});
-
-
-
-app.get("/botCallback", function (req, res) {
-
-    console.log("Bot API callback received at:", Date(), Date.now());
-  
-    //console.log(
-     // "Printing parameters received for /botCallback (GET) ",
-    //  req.query
-   // );
-    console.log(
-      "Response Body (GET): ",
-      req.body
-    );
-  
-    res.status = 200;
-  
-    // send 200 OK response
-    res.header("Content-Type", "text/plain");
-    res.send("Bot API callback response received !!!");
-  });
-  
-  app.post("/botCallback", function (req, res) {
-  
-    callback_time =  Date.now();
-    console.log("Bot API callback received count:", counter++, Date(), Date.now());
-    console.log("Callback time:", callback_time-end_time);
-  
-  /*  console.log(
-      "Response Body (POST): ",
-      req.body
-    );
-  */
-    // send 200 OK response
-    res.header("Content-Type", "text/plain");
-    res.send("Bot API callback response received !!!");
-  });
-  
-
-  
-
-
 const newPost = {
     "business_id": 1717,
     "user": {
@@ -105,7 +22,7 @@ function myFunction () {
 
         start_time =  Date.now();
         console.log("            ");
-        console.log("Start time is:", Date(), start_time);
+        console.log("T1:: API request send time is:", Date(), start_time);
 
         axios.post(
                 "https://jio-messenger.hellohaptik.com/v1.0/log_message_from_user/",
@@ -115,8 +32,8 @@ function myFunction () {
         //.then((response) => console.log(response.data))
     .then((response) => countRsp(start_time))
         .catch((error) => console.log(error.response));
-}
 
+}
 
 function countRsp(start_time){
     if ( typeof countRsp.counter == 'undefined' ) {
@@ -132,18 +49,9 @@ function countRsp(start_time){
         //console.log("MAX_COUNT reached", countRsp.counter);
         //console.log("Start time:", start_time);
         //console.log("End time is:", Date(), end_time);
-        console.log("Endtime is: " + Date(), end_time + " Time taken (ms):", end_time-start_time);
+        console.log("T2:: First API response received: " + Date(), end_time + " Time taken from API request (ms):", end_time-start_time);
 
 //console.log("Timer:", i);
 sleep(1000).then(() => {
     myFunction();
 })
-
-
-    }
-
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
